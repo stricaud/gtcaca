6c1d182951fc307f83fab54b1afc3ebe2320a3e9
Sebastien Tricaud <sebastien@honeynet.org>
2016-05-18 22:42:52 -0700

    Add the pkg-config entry

---
+++ gtcaca.pc.cmake
@@ -0,0 +1,8 @@
+Name: gtcaca
+Description: Library to create text-based interfaces using widgets
+Version: ${GTCACA_VERSION}
+Requires: caca >= 0.99
+Conflicts:
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX} -lgtcaca
+Libs.private: @ZLIB_LIBS@
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
